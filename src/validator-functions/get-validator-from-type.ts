fc7fd16ad714e1599901077521f92a4fe4b1db65
nicolas8991@gmail.com <nicolas8991@gmail.com>
2021-07-01 18:33:00 +0200

    Fix ajv validator type signature

--- src/validator-functions/get-validator-from-type.ts
+++ src/validator-functions/get-validator-from-type.ts
@@ -1,7 +1,7 @@
 import Ajv, { Options, ValidateFunction } from 'ajv'
 import { Config } from "ts-json-schema-generator";
 
-export function getValidatorFromType<_T> (schemaGenerationOptions?: Omit<Config, "tsconfig">, ajvOptions?: Options): ValidateFunction {
+export function getValidatorFromType<T> (schemaGenerationOptions?: Omit<Config, "tsconfig">, ajvOptions?: Options): ValidateFunction<T> {
     const ajv = new Ajv(ajvOptions)
-    return ajv.compile(schemaGenerationOptions as any)
+    return ajv.compile<T>(schemaGenerationOptions as any)
 }
